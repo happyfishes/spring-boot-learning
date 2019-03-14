24ef5be49b50c98d36de161c28b4979393b45ba3
happyfishes <ghbstart@163.com>
2019-03-14 17:58:46 +0800

    spring-boot-multi-jdbc

---
+++ spring-boot-multi-jdbc/src/main/java/com/itea/boot/MultiJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.itea.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiJdbcApplication.class, args);
+    }
+
+}
