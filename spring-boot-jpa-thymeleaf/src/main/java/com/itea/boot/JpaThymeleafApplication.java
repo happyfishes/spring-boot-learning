83578059d3eead8e6cea8e570833f5a828f7d9a0
happyfishes <ghbstart@163.com>
2019-03-16 19:19:02 +0800

    spring-boot-jps-thymeleaf

---
+++ spring-boot-jpa-thymeleaf/src/main/java/com/itea/boot/JpaThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.itea.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaThymeleafApplication.class, args);
+    }
+
+}
